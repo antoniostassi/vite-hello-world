5bf28c759493bb5a07463003c91d47c037af54a3
antoniostassi <stassiant@outlook.it>
2024-09-09 14:39:43 +0200

    + Components Title / Image

---
+++ src/components/AppHero.vue
@@ -0,0 +1,14 @@
+<script>
+
+</script>
+
+<template>
+  <img src="/public/vite.svg" alt="Logo">
+</template>
+
+<style scoped>
+    img {
+        width:250px;
+        text-align:center;
+    }
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  data() {
+    return { 
+      title:"La mia prima app con Vue + Vite!"
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<style scoped>
+
+h1 {
+  color: lightcoral;
+}
+
+</style>
