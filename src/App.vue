8bfe512e3e1719d7f542d987cb3f35cac949c67c
antoniostassi <stassiant@outlook.it>
2024-09-09 14:36:51 +0200

    Initialising Template

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+
+
+
+<script>
+import TemplateComponent from './components/TemplateComponent.vue';
+// my-component.js
+export default {
+  data() {
+    return { 
+      count: 0 
+    }
+  },
+  components:{
+    TemplateComponent
+  }
+}
+</script>
+
+<template>
+  <TemplateComponent />
+  <h1>{{ count }}</h1>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/TemplateComponent.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  data() {
+    return { 
+      message: 'Template Vite + Vue'
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+</template>
+
+<style scoped>
+
+h1 {
+  color: lightcoral;
+}
+</style>
